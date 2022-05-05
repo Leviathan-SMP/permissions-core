6400788f15b191e6bab6c2808441689eed98e831
Mazin Habib <mazin.habib@live.com>
2022-05-05 11:18:44 -0400

    - Added DEMIGOD Rank, Colour is Dark Purple - /Rank Reset In Progress

--- src/main/java/net/leviathan/permissioncore/Rank.java
+++ src/main/java/net/leviathan/permissioncore/Rank.java
@@ -4,13 +4,14 @@ import org.bukkit.ChatColor;
 
 public enum Rank {
     OWNER(ChatColor.DARK_RED + "" + ChatColor.BOLD + "OWNER" + ChatColor.DARK_RED + " ", new String[]{""}),
+    DEMIGOD(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "DEMIGOD" + ChatColor.DARK_PURPLE + " ", new String[]{""}),
     ADMIN(ChatColor.RED + "" +ChatColor.BOLD + "ADMIN" + ChatColor.RED + " ", new String[]{""}),
     MODERATOR(ChatColor.DARK_GREEN + "" + ChatColor.BOLD + "MOD" + ChatColor.GREEN + " ", new String[]{""}),
     HELPER(ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "HELPER" + ChatColor.BLUE + " ", new String[]{""}),
 
     PATREON(ChatColor.DARK_BLUE + "", new String[]{}),
     MEMBER(ChatColor.DARK_GRAY + "" + ChatColor.BOLD + "MEMBER" + ChatColor.GRAY + " ", new String[]{});
-
+    // remove helper, mod.  temporary for now
     private String display;
     private String[] permissions;
 
